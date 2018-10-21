44bb0523c8670306cc000fbc54f0f0b4dec5c6ef
Thomas Gleason <Thomas.Gleason@gcu.edu>
2018-10-21 05:40:49 -0700

    added startup file

--- install_survey.sh
+++ install_survey.sh
@@ -9,7 +9,7 @@ fi
 git clone https://github.com/2018-Arizona-Opportunity-Hack/Team7.git
 ##
 docker build --rm -t survey-stack Team7/
-#cp Team7/start_survey /bin
-#chmod +x /bin/start_survey
+cp Team7/start-survey /bin/
+chmod +x /bin/start-survey
 rm -rf Team7
-echo "Server built."
+echo "Server built. Run the start-survey command to start the server"
