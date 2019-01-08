cc840f58ef6248b9c5a440f0fa643ed41954fc0f
zzyphor <zzyphor@debian-s-2vcpu-4gb-sfo2-01.localdomain>
2019-01-08 01:30:52 +0000

    Removed other sql file, updated install script

--- install_survey.sh
+++ install_survey.sh
@@ -1,23 +1,4 @@
-if [ $(dpkg-query -W -f='${Status}' docker 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  apt-get update -y
-  apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-  apt-get update -y
-  apt-get install -y docker-ce
-fi
-if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  apt-get install -y git;
-fi
-#git clone https://github.com/2018-Arizona-Opportunity-Hack/Team7.git
-#cd Team7/
-#git checkout additional
-#cd ../
-##
-docker build --rm -t survey-stack . #Team7/
+sudo docker build -t survey-stack .
 cp start-survey /bin/
 chmod +x /bin/start-survey
-#rm -rf Team7
-echo "Server built. Run the start-survey command to start the server"
+echo "Server built. Run the `start-survey` command to start the server"
